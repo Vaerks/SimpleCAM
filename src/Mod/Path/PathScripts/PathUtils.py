a44e22c85d5bd4c68ef7de0931b6d5a27f0668ad
Peter <peterxp@hotmail.fr>
2019-03-15 16:51:07 +0100

    Envelope fix for Contour

--- src/Mod/Path/PathScripts/PathUtils.py
+++ src/Mod/Path/PathScripts/PathUtils.py
@@ -42,7 +42,7 @@ from PySide import QtGui
 
 import copy
 
-if True:
+if False:
     PathLog.setLevel(PathLog.Level.DEBUG, PathLog.thisModule())
     PathLog.trackModule(PathLog.thisModule())
 else:
@@ -406,7 +406,6 @@ def getEnvelope(partshape, subshape=None, depthparams=None):
     # Extrude the section to top of Boundbox or desired height
     envelopeshape = sec.extrude(FreeCAD.Vector(0, 0, eLength))
     if PathLog.getLevel(PathLog.thisModule()) == PathLog.Level.DEBUG:
-        # TODO: Find another way to remove the removalshape after creating the operation
         removalshape = FreeCAD.ActiveDocument.addObject("Part::Feature", "Envelope")
         removalshape.Shape = envelopeshape
     return envelopeshape
