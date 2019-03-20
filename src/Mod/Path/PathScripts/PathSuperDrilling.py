dd4d2b6a01e7f32153ee5f83c217056df8aced15
Peter <peterxp@hotmail.fr>
2019-03-20 14:08:53 +0100

    Super Drilling Operation files re-added

---
+++ src/Mod/Path/PathScripts/PathSuperDrilling.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2014 Yorik van Havre <yorik@uncreated.net>              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+from __future__ import print_function
+
+import ArchPanel
+import FreeCAD
+import FreeCADGui
+import Path
+import PathScripts.PathCircularHoleBase as PathCircularHoleBase
+import PathScripts.PathLog as PathLog
+import PathScripts.PathOp as PathOp
+import PathScripts.PathUtils as PathUtils
+
+# Imports for Sub-operations creation
+import PathScripts.PathDrilling as PathDrilling
+import PathScripts.PathHelix as PathHelix
+
+from PathScripts.PathUtils import fmt, waiting_effects
+from PySide import QtCore
+
+__title__ = "Path Drilling Super Operation"
+__author__ = "MH Tech"
+__url__ = "http://www.vaerks.com"
+__doc__ = "Super operation."
+
+if False:
+    PathLog.setLevel(PathLog.Level.DEBUG, PathLog.thisModule())
+    PathLog.trackModule(PathLog.thisModule())
+else:
+    PathLog.setLevel(PathLog.Level.INFO, PathLog.thisModule())
+
+
+# Qt tanslation handling
+def translate(context, text, disambig=None):
+    return QtCore.QCoreApplication.translate(context, text, disambig)
+
+
+class ObjectSuperDrilling(PathDrilling.ObjectDrilling):
+    '''Proxy object for Drilling operation.'''
+
+    def circularHoleFeatures(self, obj):
+        '''circularHoleFeatures(obj) ... drilling works on anything, turn on all Base geometries and Locations.'''
+        return PathOp.FeatureBaseGeometry | PathOp.FeatureLocations
+
+    def initCircularHoleOperation(self, obj):
+        '''initCircularHoleOperation(obj) ... add drilling specific properties to obj.'''
+        obj.addProperty("App::PropertyLength", "PeckDepth", "Drill", QtCore.QT_TRANSLATE_NOOP("App::Property", "Incremental Drill depth before retracting to clear chips"))
+        obj.addProperty("App::PropertyBool", "PeckEnabled", "Drill", QtCore.QT_TRANSLATE_NOOP("App::Property", "Enable pecking"))
+        obj.addProperty("App::PropertyFloat", "DwellTime", "Drill", QtCore.QT_TRANSLATE_NOOP("App::Property", "The time to dwell between peck cycles"))
+        obj.addProperty("App::PropertyBool", "DwellEnabled", "Drill", QtCore.QT_TRANSLATE_NOOP("App::Property", "Enable dwell"))
+        obj.addProperty("App::PropertyBool", "AddTipLength", "Drill", QtCore.QT_TRANSLATE_NOOP("App::Property", "Calculate the tip length and subtract from final depth"))
+        obj.addProperty("App::PropertyEnumeration", "ReturnLevel", "Drill", QtCore.QT_TRANSLATE_NOOP("App::Property", "Controls how tool retracts Default=G98"))
+
+        obj.addProperty("App::PropertyInteger", "TestProperty", "XXX",
+                        QtCore.QT_TRANSLATE_NOOP("App::Property", "Just a test")) # Test Property
+
+        ################################################################################################
+        # Super Operations properties:
+        # Link property: Each sub-operation needs be linked to the Super Operation in order to allow it
+        # to edit those correctly if needed by the user from the GUI
+        obj.addProperty("App::PropertyLink", "SuperDrillingOperation", "SuperOperation",
+                        QtCore.QT_TRANSLATE_NOOP("App:Property", "Sub-operations attribute."))
+
+        obj.addProperty("App::PropertyBool", "DrillingAutoSuggest", "SuperOperation",
+                        QtCore.QT_TRANSLATE_NOOP("App:Property", "Drilling tool auto suggestion"))
+        obj.addProperty("App::PropertyBool", "HoleMillingAutoSuggest", "SuperOperation",
+                        QtCore.QT_TRANSLATE_NOOP("App:Property", "Hole Milling tool auto suggestion"))
+        obj.addProperty("App::PropertyBool", "ThreadMillingAutoSuggest", "SuperOperation",
+                        QtCore.QT_TRANSLATE_NOOP("App:Property", "Thread Milling tool auto suggestion"))
+        ################################################################################################
+
+        obj.ReturnLevel = ['G98', 'G99']  # this is the direction that the Contour runs
+
+        obj.addProperty("App::PropertyDistance", "RetractHeight", "Drill", QtCore.QT_TRANSLATE_NOOP("App::Property", "The height where feed starts and height during retract tool when path is finished"))
+
+        obj.DrillingAutoSuggest = True
+        obj.HoleMillingAutoSuggest = True
+        obj.ThreadMillingAutoSuggest = True
+
+    def circularHoleExecute(self, obj, holes):
+        '''circularHoleExecute(obj, holes) ... generate drill operation for each hole in holes.'''
+        PathLog.track()
+
+        self.commandlist.append(Path.Command("(Begin SuperDrilling)"))
+
+        # rapid to clearance height
+        self.commandlist.append(Path.Command('G0', {'Z': obj.ClearanceHeight.Value, 'F': self.vertRapid}))
+
+        tiplength = 0.0
+        if obj.AddTipLength:
+            tiplength = PathUtils.drillTipLength(self.tool)
+
+        holes = PathUtils.sort_jobs(holes, ['x', 'y'])
+        self.commandlist.append(Path.Command('G90'))
+        self.commandlist.append(Path.Command(obj.ReturnLevel))
+
+        cmd = "G81"
+        cmdParams = {}
+        cmdParams['Z'] = obj.FinalDepth.Value - tiplength
+        cmdParams['F'] = self.vertFeed
+        cmdParams['R'] = obj.RetractHeight.Value
+        if obj.PeckEnabled and obj.PeckDepth.Value > 0:
+            cmd = "G83"
+            cmdParams['Q'] = obj.PeckDepth.Value
+        elif obj.DwellEnabled and obj.DwellTime > 0:
+            cmd = "G82"
+            cmdParams['P'] = obj.DwellTime
+
+        for p in holes:
+            params = {}
+            params['X'] = p['x']
+            params['Y'] = p['y']
+            params.update(cmdParams)
+            self.commandlist.append(Path.Command(cmd, params))
+
+        self.commandlist.append(Path.Command('G80'))
+
+    def opSetDefaultValues(self, obj):
+        '''opSetDefaultValues(obj) ... set default value for RetractHeight'''
+        obj.RetractHeight = 10
+
+
+def Create(name):
+    superop = FreeCAD.ActiveDocument.addObject("Path::FeatureCompoundPython", name)
+    proxy = ObjectSuperDrilling(superop)
+    if superop.Proxy:
+        proxy.findAllHoles(superop)
+
+    # Creating sub-operations
+    op_drill1 = FreeCAD.ActiveDocument.addObject("Path::FeaturePython", "sub_"+superop.Name + "_drill_center")
+    op_drill2 = FreeCAD.ActiveDocument.addObject("Path::FeaturePython", "sub_"+superop.Name + "_drill_base")
+    op_helix1 = FreeCAD.ActiveDocument.addObject("Path::FeaturePython", "sub_"+superop.Name + "_helix_holemilling")
+
+    # Adding sub-objects to super operation. Objects must be added before initialization to avoid
+    #  being claimed by Job.Operations.
+    superop.Group = [op_drill1, op_drill2, op_helix1]
+
+    drill1 = PathDrilling.ObjectDrilling(op_drill1)
+    drill2 = PathDrilling.ObjectDrilling(op_drill2)
+    helix1 = PathHelix.ObjectHelix(op_helix1)
+
+    # To select all edges of a hole:
+    if len(FreeCADGui.Selection.getSelectionEx()) > 0:
+        selection = FreeCADGui.Selection.getSelectionEx()[0]
+        PathUtils.selectAllLoops(selection)
+
+    return superop
---
+++ src/Mod/Path/PathScripts/PathSuperDrillingGui.py
@@ -0,0 +1,242 @@
+# -*- coding: utf-8 -*-
+
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2017 sliptonic <shopinthewoods@gmail.com>               *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+import FreeCAD
+import FreeCADGui
+import PathScripts.PathCircularHoleBaseGui as PathCircularHoleBaseGui
+import PathScripts.PathGui as PathGui
+import PathScripts.PathLog as PathLog
+import PathScripts.PathOpGui as PathOpGui
+
+import copy
+
+from PySide import QtCore, QtGui
+
+from PathScripts import PathSuperDrilling
+
+# Imports for Sub-operations creation
+from PathScripts import PathDrillingGui
+from PathScripts import PathHelixGui
+
+from PathScripts import PathUtils
+
+__title__ = "Path Drilling Super Operation"
+__author__ = "MH Tech"
+__url__ = "http://www.vaerks.com"
+__doc__ = "Super operation."
+
+if False:
+    PathLog.setLevel(PathLog.Level.DEBUG, PathLog.thisModule())
+    PathLog.trackModule(PathLog.thisModule())
+else:
+    PathLog.setLevel(PathLog.Level.NOTICE, PathLog.thisModule())
+
+
+class TaskPanelOpPage(PathCircularHoleBaseGui.TaskPanelOpPage):
+    '''Controller for the drilling operation's page'''
+
+    def getBaseGeometryForm(self):
+        return FreeCADGui.PySideUic.loadUi(":/panels/PageBaseHoleGeometryEdit.ui")
+
+    def getForm(self):
+        '''getForm() ... return UI'''
+        return FreeCADGui.PySideUic.loadUi(":/panels/PageOpSuperDrillingEdit.ui")
+
+    def getFields(self, obj):
+        '''setFields(obj) ... update obj's properties with values from the UI'''
+        PathLog.track()
+        PathGui.updateInputField(obj, 'PeckDepth', self.form.peckDepth)
+        PathGui.updateInputField(obj, 'RetractHeight', self.form.retractHeight)
+        PathGui.updateInputField(obj, 'DwellTime', self.form.dwellTime)
+
+        if obj.DwellEnabled != self.form.dwellEnabled.isChecked():
+            obj.DwellEnabled = self.form.dwellEnabled.isChecked()
+        if obj.PeckEnabled != self.form.peckEnabled.isChecked():
+            obj.PeckEnabled = self.form.peckEnabled.isChecked()
+        if obj.AddTipLength != self.form.useTipLength.isChecked():
+            obj.AddTipLength = self.form.useTipLength.isChecked()
+
+        # For sub-operations parameter:
+        if obj.Group[2].Active != self.form.basehelix_active.isChecked():
+            obj.Group[2].Active = self.form.basehelix_active.isChecked()
+
+        if obj.DrillingAutoSuggest != self.form.basedrill_autosuggest.isChecked():
+            obj.DrillingAutoSuggest = self.form.basedrill_autosuggest.isChecked()
+        if obj.HoleMillingAutoSuggest != self.form.basehelix_autosuggest.isChecked():
+            obj.HoleMillingAutoSuggest = self.form.basehelix_autosuggest.isChecked()
+        if obj.ThreadMillingAutoSuggest != self.form.thread_autosuggest.isChecked():
+            obj.ThreadMillingAutoSuggest = self.form.thread_autosuggest.isChecked()
+
+        for subobj in obj.Group:
+            name = subobj.Name.split("_")
+            opname = name[2]
+            oplocation = name[3]
+
+            if opname == "drill" and oplocation == "base":
+                self.updateSuggestionToolController(subobj, self.form.basedrill_tool)
+
+            elif opname == "helix":
+                self.updateSuggestionToolController(subobj, self.form.basehelix_tool)
+
+        self.setSuggestedToolFields()
+
+
+    def setFields(self, obj):
+        '''setFields(obj) ... update UI with obj properties' values'''
+        PathLog.track()
+
+        self.form.peckDepth.setText(FreeCAD.Units.Quantity(obj.PeckDepth.Value, FreeCAD.Units.Length).UserString)
+        self.form.retractHeight.setText(FreeCAD.Units.Quantity(obj.RetractHeight.Value, FreeCAD.Units.Length).UserString)
+        self.form.dwellTime.setText(str(obj.DwellTime))
+
+        if obj.DwellEnabled:
+            self.form.dwellEnabled.setCheckState(QtCore.Qt.Checked)
+        else:
+            self.form.dwellEnabled.setCheckState(QtCore.Qt.Unchecked)
+
+        if obj.PeckEnabled:
+            self.form.peckEnabled.setCheckState(QtCore.Qt.Checked)
+        else:
+            self.form.peckEnabled.setCheckState(QtCore.Qt.Unchecked)
+
+        if obj.AddTipLength:
+            self.form.useTipLength.setCheckState(QtCore.Qt.Checked)
+        else:
+            self.form.useTipLength.setCheckState(QtCore.Qt.Unchecked)
+
+        # For sub-operations parameter
+        if obj.Group[2].Active:
+            self.form.basehelix_active.setCheckState(QtCore.Qt.Checked)
+        else:
+            self.form.basehelix_active.setCheckState(QtCore.Qt.UnChecked)
+
+        if obj.DrillingAutoSuggest:
+            self.form.basedrill_autosuggest.toggle()
+        if obj.HoleMillingAutoSuggest:
+            self.form.basehelix_autosuggest.toggle()
+        if obj.ThreadMillingAutoSuggest:
+            self.form.thread_autosuggest.toggle()
+
+        self.suggestionsChangeState()
+
+        # Change the SuggestionToolControllers GUI
+        self.setSuggestedToolFields()
+
+    def getHoleDiameter(self, obj):
+        for i, (base, subs) in enumerate(obj.Base):
+            for sub in subs:
+                return obj.Proxy.holeDiameter(obj, base, sub)
+
+    def setSuggestedToolFields(self):
+        obj = self.obj
+        holediameter = self.getHoleDiameter(obj)
+
+        for subobj in obj.Group:
+            self.updateSuggestedTool(subobj, holediameter)
+
+    def updateSuggestedTool(self, subobj, holediameter):
+        name = subobj.Name.split("_")
+        opname = name[2]
+        oplocation = name[3]
+
+        tc = PathUtils.getToolControllers(subobj)
+
+        if opname == "drill" and oplocation == "center":
+            subobj.ToolController = PathUtils.filterToolControllers(tc, "CenterDrill")[0]
+
+        elif opname == "chamfering":
+            subobj.ToolController = PathUtils.filterToolControllers(tc, "ChamferMill")[0]
+
+        elif opname == "drill" and oplocation == "base":
+            toolslist = PathUtils.getAllSuggestedTools(
+                                              PathUtils.filterToolControllers(tc, "Drill"), holediameter)
+
+            if self.form.basedrill_autosuggest.isChecked() is False \
+                    and toolslist.__contains__(subobj.ToolController):
+                toolslist.remove(subobj.ToolController)
+                toolslist.insert(0, subobj.ToolController)
+
+            self.setupSuggestedToolController(subobj, self.form.basedrill_tool, toolslist)
+
+        elif opname == "helix":
+            toolslist = PathUtils.getAllSuggestedTools(
+                PathUtils.filterToolControllers(tc, "EndMill"), holediameter)
+
+            if self.form.basehelix_autosuggest.isChecked() is False \
+                    and toolslist.__contains__(subobj.ToolController):
+                toolslist.remove(subobj.ToolController)
+                toolslist.insert(0, subobj.ToolController)
+
+            self.setupSuggestedToolController(subobj, self.form.basehelix_tool, toolslist)
+
+
+    def getSignalsForUpdate(self, obj):
+        '''getSignalsForUpdate(obj) ... return list of signals which cause the receiver to update the model'''
+        signals = []
+
+        signals.append(self.form.retractHeight.editingFinished)
+        signals.append(self.form.peckDepth.editingFinished)
+        signals.append(self.form.dwellTime.editingFinished)
+        signals.append(self.form.dwellEnabled.stateChanged)
+        signals.append(self.form.peckEnabled.stateChanged)
+        signals.append(self.form.useTipLength.stateChanged)
+
+        signals.append(self.form.basedrill_tool.currentIndexChanged)
+        signals.append(self.form.basehelix_tool.currentIndexChanged)
+
+        signals.append(self.form.basehelix_active.stateChanged)
+
+        self.form.basedrill_autosuggest.clicked.connect(self.suggestionsChangeState)
+        self.form.basehelix_autosuggest.clicked.connect(self.suggestionsChangeState)
+        self.form.thread_autosuggest.clicked.connect(self.suggestionsChangeState)
+
+        return signals
+
+    def suggestionsChangeState(self):
+        self.updateState(self.form.basedrill_tool, self.form.basedrill_autosuggest)
+        self.updateState(self.form.basehelix_tool, self.form.basehelix_autosuggest)
+        self.updateState(self.form.thread_tool, self.form.thread_autosuggest)
+
+        self.setSuggestedToolFields()
+
+    def updateState(self, combo, button):
+        if hasattr(button, "isChecked"):
+            combo.setEnabled(not button.isChecked())
+
+
+
+# The subCmdResources has to be manually updated by adding a new Resource type when a new sub-operation
+# has to be done in the Super Operation procedure
+subCmdResources = [PathDrillingGui.Resource, PathDrillingGui.Resource, PathHelixGui.Resource]
+Resource = PathOpGui.CommandResources('SuperDrilling',
+        PathSuperDrilling.Create,
+        TaskPanelOpPage,
+        'Path-SuperDrilling',
+        QtCore.QT_TRANSLATE_NOOP("PathSuperDrilling", "SuperDrilling"),
+        QtCore.QT_TRANSLATE_NOOP("PathSuperDrilling", "SuperDrilling Super Operation"),
+        None)
+
+Command = PathOpGui.SetupOperation(Resource, subCmdResources)
+
+FreeCAD.Console.PrintLog("Loading PathSuperDrillingGui... done\n")
